2b5e8fc00822d486ee9682c1d1db95b8a78eb25b
Olga <xotaoshi@gmail.com>
2026-05-26 00:06:11 +0300

    changed to get env

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 	- читать информацию о книге из файла 
 	- работать с каталогом /app/data, в котором хранятся данные 
 '''
+
+
 import os
 
 class Book:
@@ -38,3 +40,22 @@ class Book:
             return Book(title, author, status)
 
 
+if __name__ == "__main__":
+
+    flag = input("Do you want to load book information from a file or read it? (r/l): ")
+    if flag.lower() == 'r':
+        filename = input("Enter the filename to load book information from: ")
+        book = Book.load_from_file(filename)
+        print(book.to_dict())
+    else:
+        book_name = input("Enter book name: ")
+        book_author = input("Enter book author: ")
+        book_status = input("Enter book status (available/borrowed): ")
+
+        book = Book(book_name, book_author, book_status)
+        filename = input("Enter the filename to save book information to: ")
+        book.save_to_file(filename)
+        print(f"Book information saved to {filename}")
+
+
+    
